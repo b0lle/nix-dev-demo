b59086091e6f38272d49eb54be42068d8d3de89f
julianfrenzel <26822719+b0lle@users.noreply.github.com>
2024-11-25 23:54:29 +0100

    chore: using nix channel 24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.05"; # nixpkgs-unstable
   inputs.systems.url = "github:nix-systems/default";
   inputs.flake-utils = {
     url = "github:numtide/flake-utils";
